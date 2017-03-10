33e4ed94ed60f82a79cb900dbcf6c5109e8f074b
AlexanderTang <alexandertang357@hotmail.com>
2017-03-10 12:42:35 +0100

    pipeline generally works

--- Python_pipeline/pipeline/pipeline_body.py
+++ Python_pipeline/pipeline/pipeline_body.py
@@ -7,25 +7,27 @@ class Pipeline:
     def continue_pipeline(self, plProgram, inferenceEngine):
         lf = pl.formula.LogicFormula.create_from(plProgram)  # ground into logic formula
         cnf = pl.cnf_formula.CNF.create_from(lf)     # get CNF
-        print(cnf)
-        if inferenceEngine is None:
-            None #TODO
-        else:
+        if inferenceEngine is None:  # use problog to solve
+            nnf = pl.nnf_formula.NNF.create_from(cnf)  # transform to nnf
+            return nnf.evaluate()   # compute conditional probabilities
+        else:  # use inference engine (miniC2D)
             None #TODO
 
 
+    # Enter a ProbLog program as a tuple: (model, evidence, queries)
     def execProbLogModel(self, probLogProgram, inferenceEngine=None):
         p = probLogProgram[0]
         if (probLogProgram[1] is not None):
             p += probLogProgram[1]
         if (probLogProgram[2] is not None):
             p += probLogProgram[2]
-        self.continue_pipeline(pl.program.PrologString(p),inferenceEngine)
+        return self.continue_pipeline(pl.program.PrologString(p),inferenceEngine)
 
 
+    # Enter the relative path to a Bayesian network file (.net extension)
     def execBayesianNetwork(self, bayesianNetwork, inferenceEngine=None):
         output_filename = "output_" + bayesianNetwork[0]  # output to "output_<input_file_name>"
         bayesianNetwork.append("-o")
         bayesianNetwork.append(output_filename)
         h2p.main(bayesianNetwork)
-        self.continue_pipeline(pl.program.PrologFile(output_filename), inferenceEngine)
+        return self.continue_pipeline(pl.program.PrologFile(output_filename), inferenceEngine)
--- Python_pipeline/pipeline/run.py
+++ Python_pipeline/pipeline/run.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python3
+
 from pipeline.probLogPrograms import ProbLogProgram
 from pipeline.pipeline_body import Pipeline
 
+miniC2D_exec_path = "/home/babyburger/Documents/prob/miniC2D-1.0.0/bin/linux/miniC2D"
+
 p = Pipeline()
 plp = ProbLogProgram()
-bn = ["alarm.net"]
 
+# bn = ["alarm.net"]
 # p.execBayesianNetwork(bn)
 
 task11 = plp.task11()
-p.execProbLogModel(task11)
+result = p.execProbLogModel(task11)
+print(result)
